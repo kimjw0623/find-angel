3ec6cccb29bea32a3e08e6ca297530b7b9702be1
jungwookim <kimjw0623@kaist.ac.kr>
2024-10-05 15:54:27 +0900

    feat: add env settings

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # find-angel
+
+## Conda를 이용한 가상환경 생성
+python -m pip install -r requirements.txt
--- main.py
+++ main.py
@@ -17,43 +17,52 @@ headers = {
 
 auction_option_url = "https://developer-lostark.game.onstove.com/auctions/options"
 
-auction_option =  requests.get(auction_option_url, headers=headers)
-with open('auction_option.json', 'w') as json_file:
-    json.dump(auction_option, json_file, indent=4, ensure_ascii=False)  # indent는 보기 좋게 들여쓰기 설정
+auction_option = requests.get(auction_option_url, headers=headers)
+auction_option_dict = json.loads(auction_option.text)
+# if not os.path.exists('auction_option.json'):
+    # with open('auction_option.json', 'w') as json_file:
+    #     json.dump(json.loads(auction_option.text), json_file, indent=4, ensure_ascii=False)  # indent는 보기 좋게 들여쓰기 설정
 
-# url = "https://developer-lostark.game.onstove.com/auctions/items"
+def get_action_options(auction_option_dict):
+    GRINDING_OPTIONS = auction_option_dict.get("EtcOptions")[6]
+    pass
 
-# data = {
-#   "ItemLevelMin": 0,
-#   "ItemLevelMax": 1800,
-#   "ItemGradeQuality": 70,
-#   "SkillOptions": [
-#     {
-#       "FirstOption": None,
-#       "SecondOption": None,
-#       "MinValue": None,
-#       "MaxValue": None
-#     }
-#   ],
-#   "EtcOptions": [
-#     {
-#       "FirstOption": 7,
-#       "SecondOption": 45,
-#       "MinValue": None,
-#       "MaxValue": None
-#     }
-#   ],
-#   "Sort": "BIDSTART_PRICE",
-#   "CategoryCode": 200000,
-#   "CharacterClass": "",
-#   "ItemTier": 4,
-#   "ItemGrade": "고대",
-#   "ItemName": "",
-#   "PageNo": 0,
-#   "SortCondition": "ASC"
-# }
 
-# response = requests.post(url, headers=headers, json=data)
+# get_action_options(auction_option)
 
-# print(response.status_code)
-# print(response.json())
+
+auction_item_url = "https://developer-lostark.game.onstove.com/auctions/items"
+
+data = {
+  "ItemLevelMin": 0,
+  "ItemLevelMax": 1800,
+  "ItemGradeQuality": 70,
+  "SkillOptions": [
+    {
+      "FirstOption": None,
+      "SecondOption": None,
+      "MinValue": None,
+      "MaxValue": None
+    }
+  ],
+  "EtcOptions": [
+    {
+      "FirstOption": 7,
+      "SecondOption": 45,
+      "MinValue": None,
+      "MaxValue": None
+    }
+  ],
+  "Sort": "BIDSTART_PRICE",
+  "CategoryCode": 200000,
+  "CharacterClass": "",
+  "ItemTier": 4,
+  "ItemGrade": "고대",
+  "ItemName": "",
+  "PageNo": 0,
+  "SortCondition": "ASC"
+}
+
+response = requests.post(auction_item_url, headers=headers, json=data)
+auction_item_dict = json.loads(response.text)
+
